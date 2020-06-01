8efa591a666f1143605e268cef488f100c3525b3
tdomy <y.tokudomi@tdomy.com>
2020-06-01 05:46:08 +0000

    modify: fix design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="{{ secure_asset('/css/inquiry.css') }}" rel="stylesheet">.
+    <link href="{{ asset('/css/inquiry.css') }}" rel="stylesheet">
     <title>Simple Form - @yield('title')</title>
     @section('script')
     @show
